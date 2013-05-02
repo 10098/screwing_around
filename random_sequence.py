730bd51e6d2e187b45263e71b06028527046776a
Grigory Javadyan <grigory.javadyan@gmail.com>
2013-05-02 04:17:40 +0000

    random sequence generator

--- random_sequence.py
+++ random_sequence.py
@@ -1,10 +1,13 @@
 import random
 import sys
 
-if len(sys.argv)  > 1:
-   length = int(sys.argv[1])
-
+def gen_random_sequence(length):
+   r = []
    for x in range(0, length):
-      print(random.randint(0, 1000), " ", end="")
-   print()
+      r.append(random.randint(0, 1000))
+   return r
+    
+if __name__ == "__main__":
+   if len(sys.argv)  > 1:
+      print(gen_random_sequence(int(sys.argv[1])))
 
