ab5383cf60f68f075fc3418d3ee84d7a4376df40
Grigory Javadyan <grigory.javadyan@gmail.com>
2013-05-03 06:14:19 +0000

    0mq stuff

--- zmq_test.cpp
+++ zmq_test.cpp
@@ -18,40 +18,44 @@ int main(int argc, char **argv)
 void zmq_client()
 {
 	zmq::context_t ctx(1);
-	zmq::socket_t sock(ctx, ZMQ_REQ);
+	zmq::socket_t sock(ctx, ZMQ_REP);
 	char *m = (char*)malloc(10);
 	strcpy(m, "mydata");
-	zmq::message_t msg(m, 10, myf, NULL);
+	zmq::message_t msg;
 	//strcpy((char*)msg.data(), "mydata");
 
 	sock.setsockopt(ZMQ_IDENTITY, "myclient", strlen("myclient"));
 	sock.connect("tcp://127.0.0.1:6699");
+   printf("LISTENING!\n");
+   sock.recv(&msg);
+   printf("%s\n", msg.data());
+   msg.rebuild(m, 10, myf, NULL);
 	sock.send(msg);
 }
 
 void zmq_server()
 {
+   char *i = (char*)malloc(8);
+   strcpy(i, "myclient");
 	zmq::context_t ctx(1);
 	zmq::socket_t sock(ctx, ZMQ_ROUTER);
 	zmq::message_t msg;
-	
+   zmq::message_t id(i, 8, myf, NULL);	
 	sock.bind("tcp://*:6699");
-	sock.recv(&msg);
-	printf("%s\n", (char*)msg.data());
-	int64_t m=34856;
-	size_t s = sizeof(m);
-	sock.getsockopt(ZMQ_RCVMORE, &m, &s); 
-	printf("%d\n", m);
-	zmq::message_t body;
-	sock.recv(&body);
-	sock.getsockopt(ZMQ_RCVMORE, &m, &s); 
-	printf("%d\n", m);
-	printf("%s\n",(char*)body.data());
-	sock.recv(&body);
-	sock.getsockopt(ZMQ_RCVMORE, &m, &s); 
-	printf("%d\n", m);
-	printf("%s\n",(char*)body.data());
-
-
+   getc(stdin);
+   printf("SENDING!\n");
+   sock.send(id, ZMQ_SNDMORE);
+   id.rebuild();
+   sock.send(id, ZMQ_SNDMORE);
+   char *m = (char*)malloc(10);
+   strcpy(m, "HEY!");
+   msg.rebuild(m, 10, myf, NULL);
+   sock.send(msg);
+   printf("RECEIVING!\n");
+   sock.recv(&id);
+   printf("%s\n", (char*)id.data());
+   sock.recv(&msg);
+   sock.recv(&msg);
+   printf("%s\n", (char*)msg.data());
 }
 
